e02b6a5c107ccddb4419bf18a259b2c835b944da
shin-aung <118516554+shin-aung@users.noreply.github.com>
2023-01-03 21:16:29 +0630

    Done modifying

--- Prac_8/box_layout_demo.py
+++ Prac_8/box_layout_demo.py
@@ -6,7 +6,12 @@ class BoxLayoutDemo(App):
     def build(self):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
+        self.root.ids.output_label.text = f"Hello {self.root.ids.output_label.text}"
         return self.root
+
+    def handle_greet(self):
+        print('greet')
+
 
 
 BoxLayoutDemo().run(
